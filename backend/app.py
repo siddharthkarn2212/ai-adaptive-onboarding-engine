36b57bbd827130f1622de5a265541312e165a3fe
siddharth karn <siddharthkarn1204@gmail.com>
2026-03-21 15:34:53 +0530

    Update app.py
    fix os

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ai_engine.main_engine import analyze
@@ -48,10 +49,6 @@ def analyze_api():
         print("ERROR:", e)
         return jsonify({"error": str(e)}), 500
 
-
 if __name__ == "__main__":
-    import os
-
-port = int(os.environ.get("PORT", 5000))
-
-app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
